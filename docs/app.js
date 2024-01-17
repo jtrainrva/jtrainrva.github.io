f30641262d1c97b4d3ebb275ff6f1db6cdae4ee5
Joseph Burris <jtrainrva@gmail.com>
2024-01-17 00:13:16 -0500

    trying new display

--- docs/app.js
+++ docs/app.js
@@ -1,50 +1,15 @@
-console.log('Is this working?');
-
-let viz;
-
-//Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/AO3_Observed_Data/ChangeWork?:language=en-US&:display_count=n&:origin=viz_share_link";
-
-
-const vizContainer = document.getElementById('vizContainer');
-const options = {
-    hideTabs: true,
-    height: 1000,
-    width: 1200,
-    onFirstInteraction: function() {
-        workbook = viz.getWorkbook();
-        activeSheet = workbook.getActiveSheet();
-        console.log("My dashboard is interactive");
-    }
-};
-
-//create a function to generate the viz element
-function initViz() {
-    console.log('Executing the initViz function!');
-    viz = new tableau.Viz(vizContainer, url, options);
-}
-
-// run the initViz function when the page loads
-document.addEventListener("DOMContentLoaded", initViz);
-
-const exportPDF = document.getElementById('exportPDF');
-const exportImage = document.getElementById('exportImage');
-
-
-//click on the pdf button to generate pdf of dashboard
-function generatePDF() {
-    viz.showExportPDFDialog()
-}
-
-exportPDF.addEventListener("click", function () {
-    generatePDF();
-  });
-
-//click on image to generate image of dashboard
-function generateImage() {
-    viz.showExportImageDialog()
-}
-
-exportImage.addEventListener("click", function () {
-    generateImage();
-  });
+function initializeViz() {
+    // JS object that points at empty div in the html
+    var placeholderDiv = document.getElementById("tableauViz");
+    // URL of the viz to be embedded
+    var url = "https://public.tableau.com/app/profile/joseph.burris/viz/AO3_Observed_Data/ChangeWork";
+    // An object that contains options specifying how to embed the viz
+    var options = {
+      width: '1200px',
+      height: '1000px',
+      hideTabs: true,
+      hideToolbar: true,
+    };
+    viz = new tableau.Viz(placeholderDiv, url, options);
+  }
+  
