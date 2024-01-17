c1ea86870851c0b0f23d47d16983c69265456ce7
Joseph Burris <jtrainrva@gmail.com>
2024-01-16 23:55:27 -0500

    changed title

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/AO3_Observed_Data/ChangeWork?:language=en-US&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/Squirrels_15746293266160/Dashboard1?:display_count=y&:origin=viz_share_link";
+https://public.tableau.com/views/AO3_Observed_Data/ChangeWork?:language=en-US&:display_count=n&:origin=viz_share_link
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
